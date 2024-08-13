a03b0d405d0c5534c7ebf794e768d7dca5acd715
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2024-08-13 11:41:37 +0800

    Modify | 修复检测

--- loader/src/main/scala/am9/olbcore/onebot/misc/Account.scala
+++ loader/src/main/scala/am9/olbcore/onebot/misc/Account.scala
@@ -3,4 +3,7 @@ package am9.olbcore.onebot.misc
 class Account(number: Long) {
   //val number: Long = number
   def getNumber: Long = number
+
+  override def toString: String = getNumber.toString
+  override def equals(obj: Any): Boolean = this.toString == obj.toString
 }
